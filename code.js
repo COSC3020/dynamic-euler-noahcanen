7b53a7868e10a5baea7ae8ec225b236d571a082b
noahcanen <ncanen@uwyo.edu>
2025-04-28 10:12:50 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,33 @@
 function factorial(n) {
+    if(n === 0) return 1
+    num = 1
+    i=1
+    while(i <= n){
+        num = i * num
+        i++
+    }
+    return num;
+}
+function factorial1(n) {
     if(n === 0) return 1;
     else return n * factorial(n - 1);
 }
+n = 10
+console.log(factorial(n),factorial1(n))
 
 function e(n) {
+    if(n === 0) return 1
+    num = 1
+    Euler = 1
+    i=1
+    while(i <= n){
+        num = i * num
+        i++
+        Euler = 1.0/num + Euler
+    }
+    return Euler
+}
+function e1(n) {
     if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    else return 1.0 / factorial1(n) + e1(n - 1);
 }
